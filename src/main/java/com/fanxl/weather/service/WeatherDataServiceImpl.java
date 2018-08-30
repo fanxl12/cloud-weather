a6eb90d5a9bfcfe8750f312af11ba7e4feab6167
fanxl12 <1964645988@qq.com>
2018-08-30 17:35:57 +0800

    1.增加redis支持,并对天气数据进行缓存处理;

--- src/main/java/com/fanxl/weather/service/WeatherDataServiceImpl.java
+++ src/main/java/com/fanxl/weather/service/WeatherDataServiceImpl.java
@@ -2,26 +2,41 @@ package com.fanxl.weather.service;
 
 import com.fanxl.weather.vo.WeatherResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * WeatherDataService 实现.
- * 
+ *
  * @since 1.0.0 2017年11月22日
- * @author <a href="https://waylau.com">Way Lau</a> 
+ * @author <a href="https://waylau.com">Way Lau</a>
  */
 @Service
 public class WeatherDataServiceImpl implements WeatherDataService {
+	private final static Logger logger = LoggerFactory.getLogger(WeatherDataServiceImpl.class);
+
 	private static final String WEATHER_URI = "http://wthrcdn.etouch.cn/weather_mini?";
-	
+
+	/**
+	 * 30 分钟
+	 */
+	private static final long TIME_OUT = 1800L;
+
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+
 	@Override
 	public WeatherResponse getDataByCityId(String cityId) {
 		String uri = WEATHER_URI + "citykey=" + cityId;
@@ -33,24 +48,36 @@ public class WeatherDataServiceImpl implements WeatherDataService {
 		String uri = WEATHER_URI + "city=" + cityName;
 		return this.doGetWeahter(uri);
 	}
-	
+
 	private WeatherResponse doGetWeahter(String uri) {
- 		ResponseEntity<String> respString = restTemplate.getForEntity(uri, String.class);
-		
+		String key = uri;
+		String strBody = null;
 		ObjectMapper mapper = new ObjectMapper();
 		WeatherResponse resp = null;
-		String strBody = null;
-		
-		if (respString.getStatusCodeValue() == 200) {
-			strBody = respString.getBody();
+		ValueOperations<String, String>  ops = stringRedisTemplate.opsForValue();
+		// 先查缓存，缓存有的取缓存中的数据
+		if (stringRedisTemplate.hasKey(key)) {
+			logger.info("Redis has data");
+			strBody = ops.get(key);
+		} else {
+			logger.info("Redis don't has data");
+			// 缓存没有，再调用服务接口来获取
+			ResponseEntity<String> respString = restTemplate.getForEntity(uri, String.class);
+
+			if (respString.getStatusCodeValue() == 200) {
+				strBody = respString.getBody();
+			}
+
+			// 数据写入缓存
+			ops.set(key, strBody, TIME_OUT, TimeUnit.SECONDS);
 		}
 
 		try {
 			resp = mapper.readValue(strBody, WeatherResponse.class);
 		} catch (IOException e) {
-			e.printStackTrace();
+			logger.error("Error!",e);
 		}
-		
+
 		return resp;
 	}
 
